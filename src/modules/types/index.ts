749a01eb7beebf4ff10287a54da307289ae929ad
Denys <chaplygindenys@gmail.com>
2022-09-23 10:18:10 +0300

    ✨ feat(type): add_type

--- src/modules/types/index.ts
+++ src/modules/types/index.ts
@@ -1,17 +1,48 @@
-export interface IUser {
+import { type } from "os";
+
+export type  Id = {
   id: number;
-  // avatar: string;
-  // firstName: string;
-  // lastName: string;
-  // infoId: number;
-  login: string;
-  // profession: string;
-  // telegram?: string;
-  // instagram?: string;
-  // linkedIn?: string;
 }
 
-interface Table {
-  id: number;
-  user: IUser;
+export type Name ={
+  name: string;
+}
+
+export type IUser = Id & {
+  login: string;
+  password: string;
+  telegram: string;
+  telegramId?: string;
+  userInfoId?: string;
+}
+
+export  type UserInfo = Id & {
+
+    avatar?: string;
+  firstName?: string;
+  lastName?: string;
+
+  profession?: string;
+  instagram?: string;
+  linkedIn?: string;
+}
+
+
+
+export type Favorite = Id & Name;
+
+export type Badge = Id & Name & {  
+url: string;
+userIds: Id[];
+}
+
+
+export type Room = Id & Name & {
+
+  tableIds: Id[];
+}
+
+export type  Table  = Id &  {
+  roomId : Id; 
+  userIds: Id[];
 }
